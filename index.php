eb868a68058f3c59b28019448326d1939d91ec8b
zittipon.t <zittipon.t@pttdigital.com>
2021-10-11 11:45:21 +0700

    add file

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- LeafletJS CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
+    <!-- Custom CSS -->
+
+    <link rel="stylesheet" href="./css/mymap.css" type="text/css" />
+
+    <title>สถาทีที่จะสอน</title>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <td colspan="2">จะเรียนที่ใหนหรออออ</td>
+        </tr>
+        <tr>
+            <td class="searchTab">
+                <h3>Find POIs</h3>
+            </td>
+            <td>
+                <div id="mapid"></div>
+            </td>
+        </tr>
+    </table>
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
+    <!-- LeafletJS JS -->
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
+    <!--Custom Js-->
+    <script src="./js/myscripts.js" type="text/javascript"></script>
+</body>
+
+
+</html>
